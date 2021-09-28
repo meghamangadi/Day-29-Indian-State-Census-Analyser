2c729c11875f2acdfdd91a26cfd0dbdcc1369a77
megha angadi <meghamangadi@gmail.com>
2021-09-28 23:24:02 +0530

    Added: Program Ability for the analyser to load the Indian States Census Information from a csv file

---
+++ src/test/java/com/bridgelabz/censusanalyser/CensusAnalyserTest.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.censusanalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.censusanalyser.model.CensusAnalyser; 
+
+public class CensusAnalyserTest {
+    private String INIDAN_CENSUS_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+
+    @Test
+    public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() {
+        CensusAnalyser censusAnalyser = new com.bridgelabz.censusanalyser.model.CensusAnalyser();
+        int count = censusAnalyser.loadIndiaCensusData(INIDAN_CENSUS_CSV_FILE_PATH);
+        Assert.assertEquals(29,count);
+    }
+}
