05d2d4a05fedead1c4f91f659a951009010cc431
megha angadi <meghamangadi@gmail.com>
2021-09-28 23:38:50 +0530

    Added: Program to Given the States Census CSV file, Check to ensure the Number of Record matches

--- src/main/java/com/bridgelabz/censusanalyser/model/CensusAnalyser.java
+++ src/main/java/com/bridgelabz/censusanalyser/model/CensusAnalyser.java
@@ -1,34 +1,29 @@
 package com.bridgelabz.censusanalyser.model;
 
  
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
-
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvToBeanBuilder;
+import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
 
     public int loadIndiaCensusData(String csvFilePath) {
-        Reader reader = null;
-        try {
-            reader = Files.newBufferedReader(Paths.get(csvFilePath));
-            CsvToBean<IndiaCensusCSV> csvToBean = new CsvToBeanBuilder<IndiaCensusCSV>(reader)
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
+            CsvToBean<IndiaCensusCSV>csvToBean = new CsvToBeanBuilder<IndiaCensusCSV>(reader)
                     .withType(IndiaCensusCSV.class)
                     .withIgnoreLeadingWhiteSpace(true)
                     .build();
-           
-            Iterator<IndiaCensusCSV> iterator = csvToBean.iterator();
-            int numOfEntries = 0;
-            while (iterator.hasNext()) {
-                numOfEntries++;
-                iterator.next();
-            }
-            return numOfEntries;
+            Iterator<IndiaCensusCSV>iterator = csvToBean.iterator();
+        
+            Iterable<IndiaCensusCSV> csvIterable = () -> iterator;
+            int count = (int) StreamSupport.stream(csvIterable.spliterator(), true).count();
+            return count;
         } catch (
                 IOException e) {
             System.out.println(e);
--- src/test/java/com/bridgelabz/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/censusanalyser/CensusAnalyserTest.java
@@ -1,17 +1,21 @@
 package com.bridgelabz.censusanalyser;
 
+ 
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.bridgelabz.censusanalyser.model.CensusAnalyser; 
+import com.bridgelabz.censusanalyser.model.CensusAnalyser;
+
+import java.io.IOException;
 
 public class CensusAnalyserTest {
     private String INIDAN_CENSUS_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
 
+    
     @Test
-    public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() { 
-        CensusAnalyser censusAnalyser = new com.bridgelabz.censusanalyser.model.CensusAnalyser();
+    public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() {
+        CensusAnalyser censusAnalyser = new CensusAnalyser();
         int count = censusAnalyser.loadIndiaCensusData(INIDAN_CENSUS_CSV_FILE_PATH);
-        Assert.assertEquals(29,count);
+        Assert.assertEquals(29, count);
     }
 }
