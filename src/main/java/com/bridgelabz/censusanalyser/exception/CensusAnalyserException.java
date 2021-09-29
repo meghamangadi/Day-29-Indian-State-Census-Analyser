6cab84f76a27650fcb2e46951f8193c5e6df7bd3
megha angadi <meghamangadi@gmail.com>
2021-09-29 19:48:33 +0530

    Added: Program of a Happy Test Case where the records are checked

--- src/main/java/com/bridgelabz/censusanalyser/exception/CensusAnalyserException.java
+++ src/main/java/com/bridgelabz/censusanalyser/exception/CensusAnalyserException.java
@@ -1,18 +1,13 @@
 package com.bridgelabz.censusanalyser.exception;
 
 public class CensusAnalyserException extends Exception {
+	  public enum ExceptionType {
+	        CENSUS_FILE_INCORRECT, CENSUS_INCORRECT_FILE_FORMAT, CENSUS_WRONG_DELIMITER;
+	    }
 
-	private static final long serialVersionUID = 1L;
-
-
-    public enum ExceptionType {
-    	CENSUS_FILE_INCORRECT, CENSUS_INCORRECT_FILE_FORMAT, CENSUS_WRONG_DELIMITER;
-    }
-
-	public ExceptionType type;
-
-	public CensusAnalyserException(String message, ExceptionType type) {
-		super(message);
-		this.type = type;
+	    public ExceptionType type;
+	    public CensusAnalyserException(String message, ExceptionType type) {
+	        super(message);
+	        this.type = type;
+	    }
 	}
-}
